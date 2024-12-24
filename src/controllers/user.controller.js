3b2c67f1bb370fb229c627caf5bb3e92f6a9d100
cafuentes <cfuentesmedina@gmail.com>
2024-12-24 01:05:06 -0300

    feat:Create Controllers and Routes for User

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,3 +16,70 @@ export const createUser = async (req, res) => {
         console.error(error);
     }
 }
+
+export const findUserById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const usuario = await User.findByPk(id);
+
+        res.status(200).json({
+            message: `Usuario con ID_ ${id} Encontrado con éxito`,
+            status: 200,
+            data: usuario
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const findAll = async (req, res) => {
+    try {
+        const usuarios = await User.findAll();
+
+        res.status(200).json({
+            message: "Usuarios encontrados",
+            status: 200,
+            data: usuarios
+        })
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const updateUserById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const data = req.body;
+
+        await User.update(data, {
+            where: { id }
+        });
+
+        res.status(200).json({
+            message: `Usuario con ID_ ${id} actualizado con éxito`,
+            status: 200,
+            data: data
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const deleteUserById = async (req, res) => {
+    try {
+        const { id } = req.params
+
+        const usuario = await User.findByPk(id);
+
+        await usuario.destroy();
+
+        res.status(200).json({
+            message: 'Usuario Eliminado con éxito',
+            status: 200,
+            
+        })
+    } catch (error) {
+        console.error(error);
+    }
+}
+
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express'
-import { createUser } from '../controllers/user.controller.js'
+import { createUser, findUserById, findAll, updateUserById, deleteUserById } from '../controllers/user.controller.js'
+
+
 
 const  router = Router()
 
 router.post('/user', createUser)
+router.get('/user/:id', findUserById)
+router.get('/user', findAll)
+router.put('/user/:id', updateUserById)
+router.delete('/user/:id', deleteUserById)
+
 
 
 export default router
