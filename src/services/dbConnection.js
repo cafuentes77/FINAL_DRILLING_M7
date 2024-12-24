5f2c3701d9d8835f0539cad09d2078bb1b30ba80
cafuentes <cfuentesmedina@gmail.com>
2024-12-23 21:09:12 -0300

    feat: Server is running on port 3000

---
+++ src/services/dbConnection.js
@@ -0,0 +1,12 @@
+import { dbConfig } from "../config/db.config.js"
+
+
+export const dbConnect = async() => {
+    try {
+        await dbConfig.authenticate();
+        console.log('Logramos conectarnos a Postgre a traves de Sequelize 🚀')
+    } catch (error) {
+        console.error('No pudimos conectarnos a la DB 🚨', error);
+        process.exit(1)
+    }
+}
---
+++ src/services/serverInit.js
@@ -0,0 +1,16 @@
+import { dbConnect } from "./dbConnection.js";
+
+export const serverInit = async (app, PORT) => {
+    try {
+        console.log('Verificando conexión a la base de datos');
+        await dbConnect()
+
+
+
+        app.listen (PORT, () => {
+            console.log(`Server is running on port ${PORT} ⚡🎆`);
+        })
+    } catch (error) {
+        console.error('Error al iniciaizar el servidor:', error);
+    }
+}
